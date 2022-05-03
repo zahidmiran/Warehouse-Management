a703f3ea0d8d2e1f2376c71eb2961942409642d9
zahidmiran <zahidmiron599@gmail.com>
2022-05-03 19:27:21 +0600

    firebase added

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1Qj7loPYDCr0omEDkVx9ZAtxcf04cKNU",
+  authDomain: "warehouse-management-a6773.firebaseapp.com",
+  projectId: "warehouse-management-a6773",
+  storageBucket: "warehouse-management-a6773.appspot.com",
+  messagingSenderId: "1081723549105",
+  appId: "1:1081723549105:web:0db207edc32c4366b73990"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
